df9171bfaaea8a9c25ce65eb42d47cd022e457bb
bbakercello <bbakercello@gmail.com>
2022-11-09 14:47:10 -0500

    anagram completed

--- practice.py
+++ practice.py
@@ -8,3 +8,14 @@ class Solution(object):
                 return True
             hashset.add(n)
         return False
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        s = sorted(s)
+        t = sorted(t)
+        if t == s:
+            return True        
