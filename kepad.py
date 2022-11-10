f22e8484bf66e2918966dd852d405e583f042d4b
bbakercello <bbakercello@gmail.com>
2022-11-10 11:41:51 -0500

    setup for algo done

--- kepad.py
+++ kepad.py
@@ -1,22 +1,48 @@
 def phoneWords(n:  int, words: list[str]):
     key = {
-        '2': ['a','b','c'],
-        '3': ['d','e','f'],
-        '4': ['g','h','i'],
-        '5': ['j','k','l'],
-        '6': ['m','n','o'],
-        '7': ['p','q','r','s'],
-        '8': ['t','u','v'],
-        '9': ['w','x','y','z']
+        '2': 'a',
+        '2': 'b',
+        '2': 'c',
+        '3': 'd',
+        '3': 'e',
+        '3': 'f',
+        '4': 'g',
+        '4': 'h',
+        '4': 'i',
+        '5': 'j',
+        '5': 'k',
+        '5': 'l',
+        '6': 'm',
+        '6': 'n',
+        '6': 'o',
+        '7': 'p',
+        '7': 'q',
+        '7': 'r',
+        '7': 's',
+        '8': 't',
+        '8': 'u',
+        '8': 'v',
+        '9': 'w',
+        '9': 'x',
+        '9': 'y',
+        '9': 'z'
     }
+    key_list = list(key.keys())
+    val_list = list(key.values())
+    
 
     nums = str(n)
     nums = nums.split()
-    
+    x = 0
     ## convert words to a number
     for i in words:
-        for j in words[i]:
-            if 
+        for j in words[x]:
+            if j in key.values():
+                j = val_list.index(j)
+                z = key_list[j]
+            print(z)
+        x+=1
+
     ## compare the two numbers
 
 print(phoneWords(n = 243948, words = ['foo','bar']))
