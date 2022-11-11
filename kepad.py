bd935eeb6550bfbcd40d62a09a9508a36ce6f813
bbakercello <bbakercello@gmail.com>
2022-11-11 09:04:53 -0500

    keypad finished

--- kepad.py
+++ kepad.py
@@ -1,55 +1,51 @@
 def phoneWords(n:  int, words: list[str]):
     key = {
-        '2': 'a',
-        '2': 'b',
-        '2': 'c',
-        '3': 'd',
-        '3': 'e',
-        '3': 'f',
-        '4': 'g',
-        '4': 'h',
-        '4': 'i',
-        '5': 'j',
-        '5': 'k',
-        '5': 'l',
-        '6': 'm',
-        '6': 'n',
-        '6': 'o',
-        '7': 'p',
-        '7': 'q',
-        '7': 'r',
-        '7': 's',
-        '8': 't',
-        '8': 'u',
-        '8': 'v',
-        '9': 'w',
-        '9': 'x',
-        '9': 'y',
-        '9': 'z'
+        'a': '2',
+        'b': '2',
+        'c': '2',
+        'd': '3',
+        'e': '3',
+        'f': '3',
+        'g': '4',
+        'h': '4',
+        'i': '4',
+        'j': '5',
+        'k': '5',
+        'l': '5',
+        'm': '6',
+        'n': '6',
+        'o': '6',
+        'p': '7',
+        'q': '7',
+        'r': '7',
+        's': '7',
+        't': '8',
+        'u': '8',
+        'v': '8',
+        'w': '9',
+        'x': '9',
+        'y': '9',
+        'z': '9',
     }
-    key_list = list(key.keys())
-    val_list = list(key.values())
-    
+   
 
     nums = str(n)
-    nums = nums.split()
+
     x = 0
     s = ''
+    array = []
+
     ## convert words to a number
     for i in range(len(words)):
         for j in words[x]:
-            if j in key.values():
-                j = val_list.index(j)
-                z = key_list[j]
-                s += f'{z}'
-            print(s)
+             if j in key.keys():
+                j = key.get(j)
+                s += f'{j}'
+        
+        if s in nums:
+            array.append(int(s))
+        s = ''
         x+=1
-        
-        
-    
+    print(array)
 
-
-
-    ## compare the two numbers
-
-print(phoneWords(n = 243948, words = ['foo','bar']))
+print(phoneWords(n = 3662277, words = ['foo','baz','bar']))
