0e29c33114ec5c0945dc7deff8bf27d13e2bc612
bbakercello <bbakercello@gmail.com>
2022-12-13 11:48:28 -0500

    extra loops have been removed

--- 3sum_better.py
+++ 3sum_better.py
@@ -9,16 +9,23 @@ def threeSum(nums: List[int]) -> List[List[int]]:
         #if i + j + k > 0, k-1
         #if i + j + k < 0, J +1 
         for i in range(0,len(nums)-2):
+            if nums[i] > 0:
+                break
+            if nums[i] == nums[i-1] and i > 0:
+                continue 
             lower = i+1
+            
             upper = len(nums) - 1
 
             while lower < upper:
                 s = nums[i] + nums[lower] + nums[upper]
                 if s == 0:
                     triplets.append((nums[i], nums[lower], nums[upper]))
-                    lower += 1
-                elif s < 0:
-                    lower += 1
+                   
+                if s <= 0:
+                    lower += 1 
+                    while(nums[lower]==nums[lower-1] and lower < upper):
+                        lower += 1
                 else:
                     upper -= 1
         return(list(set(triplets)))
