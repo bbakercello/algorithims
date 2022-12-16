30c14cd3500a9f472d8ee48608026eef9625d357
bbakercello <bbakercello@gmail.com>
2022-12-16 15:36:53 -0500

    code works except for values like IV

--- intToRoman.py
+++ intToRoman.py
@@ -1,13 +1,19 @@
 def intToRoman(num: int) -> str:
-    map = {1000:'M',500:'D',100:'C',50:'C',10:'X',5:'V',1:'I'}
+    map = {1000:'M',500:'D',100:'C',50:'L',10:'X',5:'V',1:'I'}
     result = ''
     
     for key in map.keys():
-        if num < key and num > 
+        # if num < key and num > 
         if num >= key:
-            result += map.get(key)   
-            num -= key
+            times = num // key
+            count = times
+            print(times)
+            num -= key * times
             print(num)
+            while count > 0:
+                result += map.get(key)
+                count -= 1   # times number of times    
+              
     return result
 
 
