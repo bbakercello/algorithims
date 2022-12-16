73bf6489014689caa3d5488bff6d7a5f887b22a8
bbakercello <bbakercello@gmail.com>
2022-12-16 16:29:00 -0500

    functionality of function almost correct. special case of digit beginning with 9 is causing error

--- intToRoman.py
+++ intToRoman.py
@@ -1,20 +1,35 @@
+from operator import index
+import math
+
 def intToRoman(num: int) -> str:
     map = {1000:'M',500:'D',100:'C',50:'L',10:'X',5:'V',1:'I'}
+   
     result = ''
     
     for key in map.keys():
-        # if num < key and num > 
-        if num >= key:
-            times = num // key
-            count = times
-            print(times)
+        print(num)
+        string = str(num)
+        times = num // key
+       
+      
+        if times == 4:
+            result += map.get(key)
+            result += map.get(key*5)
             num -= key * times
-            print(num)
+        
+        elif string[0] == '9' and num > key:
+            print('yes')
+            result += map.get(key)
+            result += map.get(key*2)
+            num -= (key + 4*(key/5))
+            num = num // 1
+        else: 
+            count = times
+            num -= key * times
             while count > 0:
                 result += map.get(key)
                 count -= 1   # times number of times    
-              
     return result
 
 
-print(intToRoman(1254))
+print(intToRoman(1994))
