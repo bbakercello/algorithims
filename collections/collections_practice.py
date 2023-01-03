84e733077837a49fcafe4b829643b859d75238f1
bbakercello <bbakercello@gmail.com>
2023-01-03 10:20:17 -0500

    leetcode mergeKLists completed

--- collections/collections_practice.py
+++ collections/collections_practice.py
@@ -74,3 +74,22 @@ def __iter__(self):
     while node is not None:
         yield node
         node = node.next
+
+
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        head = tmp = ListNode()
+        arr = []
+
+        for l in lists:
+            while l != None:
+                arr.append(l.val)
+                l = l.next
+
+        for val in sorted(arr):
+            tmp.next = ListNode()
+            tmp = tmp.next
+            tmp.val = val
+
+        return head.next
