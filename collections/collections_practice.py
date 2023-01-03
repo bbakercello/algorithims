6eabb19cbf2f895d48b82470b51756746cb960ac
bbakercello <bbakercello@gmail.com>
2023-01-03 09:37:06 -0500

    working with deque and linked lists

---
+++ collections/collections_practice.py
@@ -0,0 +1,11 @@
+from collections import deque
+
+# creates an empty linked list 
+deque()
+
+# to insert items at time of creation, give it iterable
+deque(['a','b','c'])
+
+# interact with the list 
+llist = deque('abcde')
+print(llist)
