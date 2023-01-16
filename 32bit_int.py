75db613c2db03b13d28229a2222074990b8cbe1b
bbakercello <bbakercello@gmail.com>
2023-01-16 12:28:49 -0500

    checked for whitespace

--- 32bit_int.py
+++ 32bit_int.py
@@ -1,14 +1,12 @@
-
-    def myAtoi(s: str) -> int:
+def myAtoi(self, s: str) -> int:
         state = ''
         start = 0
         end = 0
         sum = 0
+        flag = False
         import string
         for i, value in enumerate(s): 
-            if s == string.whitespace:
-                continue
-            elif value == '-' and int(s[i+1]):
+            if value == '-' and int(s[i+1]):
                 state = '-'
             elif value == '+' and int(s[i+1]):
                 state = '+'
@@ -20,6 +18,8 @@
             elif value.isdigit() and s[i+1].isdigit() == False:
                 end = i
                 sum += 1
+            elif i == start and s[i-1] == ' ':
+                flag = True
 
         if sum > 31:
             end = start + 31
@@ -27,4 +27,7 @@
         result = int(s[start : start + sum])
         if state == '-':
             result = -abs(result)
+        if flag == True:
+            return 0
         return(result)
+
