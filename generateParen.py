2c1ad901954364e44ddab97ba0d16eee9a9a58b0
bbakercello <bbakercello@gmail.com>
2023-01-17 09:23:32 -0500

    generate paren solved

---
+++ generateParen.py
@@ -0,0 +1,23 @@
+ def generateParenthesis(self, n: int) -> List[str]:
+        # can't start with closed paren
+        # if close is < open, you can add close, or add open if open is > n
+        stack = []
+        res = []
+
+        def backtrack(openN, closedN):
+            if openN == closedN == n:
+                res.append("".join(stack))
+                return
+            
+            if openN < n:
+                stack.append("(")
+                backtrack(openN + 1, closedN)
+                stack.pop()
+            
+            if closedN < openN:
+                stack.append(")")
+                backtrack(openN, closedN + 1)
+                stack.pop()
+
+        backtrack(0,0)
+        return res
